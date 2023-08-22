87bfd2d1b1baa2589fbe53f21bfe1e187b322b6a
Ashures <ashuresyt@gmail.com>
2023-08-22 18:48:22 +1200

    Adjusted pageNo display at max pages

--- app/src/ChangePokemonButtons.jsx
+++ app/src/ChangePokemonButtons.jsx
@@ -1,12 +1,12 @@
 export default function ChangePokemonButtons({prevPokemon, nextPokemon, pageNo, totalPokemon}) {
     return (
         <div className="change-pokemon">
-            <button onClick={prevPokemon} id="prev-pokemon">Prev</button>
+            <button onClick={prevPokemon} id="prev-pokemon">prev</button>
             <div id="change-page-descriptions">
-                <p>Page: {pageNo}</p>
-                <p>{pageNo * 20} / {totalPokemon}</p>
+                <p>page: {pageNo}</p>
+                <p>{pageNo * 20 > totalPokemon? totalPokemon : pageNo * 20} / {totalPokemon}</p>
             </div>
-            <button onClick={nextPokemon} id="next-pokemon">Next</button>
+            <button onClick={nextPokemon} id="next-pokemon">next</button>
         </div>
     );
 }
