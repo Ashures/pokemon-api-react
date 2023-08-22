b1f4895e462d8f4c4a572e4e43c5cf392a9195f4
Ashures <ashuresyt@gmail.com>
2023-08-22 18:53:32 +1200

    Changed prev/next page button text to arrows

--- app/src/ChangePokemonButtons.jsx
+++ app/src/ChangePokemonButtons.jsx
@@ -1,12 +1,12 @@
 export default function ChangePokemonButtons({prevPokemon, nextPokemon, pageNo, totalPokemon}) {
     return (
         <div className="change-pokemon">
-            <button onClick={prevPokemon} id="prev-pokemon">PREV</button>
+            <button onClick={prevPokemon} id="prev-pokemon">{"<"}</button>
             <div id="change-page-descriptions">
                 <p className="bold">{pageNo}</p>
                 <p>{pageNo * 20 > totalPokemon? totalPokemon : pageNo * 20} / {totalPokemon}</p>
             </div>
-            <button onClick={nextPokemon} id="next-pokemon">NEXT</button>
+            <button onClick={nextPokemon} id="next-pokemon">{">"}</button>
         </div>
     );
 }
