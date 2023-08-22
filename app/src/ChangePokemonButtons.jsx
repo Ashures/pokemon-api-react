e967601df2557a119052d7e26d0b1e340ec8f206
Ashures <ashuresyt@gmail.com>
2023-08-22 18:48:38 +1200

    Adjusted style of prev/next page buttons

--- app/src/ChangePokemonButtons.jsx
+++ app/src/ChangePokemonButtons.jsx
@@ -1,12 +1,12 @@
 export default function ChangePokemonButtons({prevPokemon, nextPokemon, pageNo, totalPokemon}) {
     return (
         <div className="change-pokemon">
-            <button onClick={prevPokemon} id="prev-pokemon">prev</button>
+            <button onClick={prevPokemon} id="prev-pokemon">PREV</button>
             <div id="change-page-descriptions">
-                <p>page: {pageNo}</p>
+                <p className="bold">{pageNo}</p>
                 <p>{pageNo * 20 > totalPokemon? totalPokemon : pageNo * 20} / {totalPokemon}</p>
             </div>
-            <button onClick={nextPokemon} id="next-pokemon">next</button>
+            <button onClick={nextPokemon} id="next-pokemon">NEXT</button>
         </div>
     );
 }
